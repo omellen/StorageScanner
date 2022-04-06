576947cb85c0ba1a02b8680669c3376d047d42f4
omellen <olivia.mellen@live.com>
2022-04-06 14:00:29 -0500

    crated getData() and more... current error is can't find mapping model for migration

--- StorageScanner/ViewController.swift
+++ StorageScanner/ViewController.swift
@@ -11,13 +11,14 @@ import CoreData
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
    
-    
     @IBOutlet weak var tableView: UITableView!
-    var tableViewData = [Storage]()
     
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var storages: [Storage] = []
     
-    override func viewDidLoad() {
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         
         tableView.delegate = self
@@ -25,67 +26,72 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "myCell")
     }
 
-    override func viewDidAppear(_ animated: Bool) {
-            
-        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Storage")
-        request.returnsObjectsAsFaults = false
-        do {
-            let result = try context.fetch(request)
-            for data in result as! [NSManagedObject]
-            {
-                tableViewData.append(data.value(forKey: "name") as! Storage)
-            }
-        }
-        catch
-            {
-                print("Failed")
-            }
+    override func viewWillAppear(_ animated: Bool)
+    {
+//        getData()
         tableView.reloadData()
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.tableViewData.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell",
-                                                    for: indexPath)
-        cell.textLabel?.text = self.tableViewData[indexPath.row].name
-        return cell
-    }
     
-    
-
-    @IBAction func WhenAddButtonPressed(_ sender: Any) {
+    @IBAction func WhenAddButtonPressed(_ sender: Any)
+    {
         let alert = UIAlertController(title: "Add Storage Area", message: "List the name of your new storage area", preferredStyle: .alert)
+        
         alert.addTextField { (textfield) in
             textfield.placeholder = "Storage Name"
-                    }
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
-            let textfield = alert.textFields![0]
+        }
+        
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        
+        let addAction = UIAlertAction(title: "Add", style: .default) { (action) in
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            let storage = Storage(context: context)
             
-            let newItem = Storage(context: self.context)
-            newItem.entity.name = textfield.text
+            let nameTFT = alert.textFields?[0].text
+            storage.name = nameTFT
             
-            do
-            {
-                try self.context.save()
-            }
-            catch
-            {}
-            
+            self.storages.append(storage)
             self.tableView.reloadData()
-            
-            
-//            if let storageText = textfield.text {
-//                self.tableViewData.append(storageText)
-//            }
-            self.tableView.reloadData()
-        }))
+            appDelegate.saveContext()
+        }
+        alert.addAction(addAction)
         self.present(alert, animated: true, completion: nil)
-        
-        
-        
+    }
+    
+    func getData()
+    {
+        if let myStorages = try? appDelegate.persistentContainer.viewContext.fetch(Storage.fetchRequest())
+        {
+            storages = myStorages
+        } else
+        {
+            printContent("error in fetching data")
+        }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return self.storages.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell",
+                                                    for: indexPath)
+        cell.textLabel?.text = self.storages[indexPath.row].name
+        return cell
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let storage = storages[indexPath.row]
+            appDelegate.persistentContainer.viewContext.delete(storage)
+            getData()
+        }
+        tableView.reloadData()
     }
     
 }
